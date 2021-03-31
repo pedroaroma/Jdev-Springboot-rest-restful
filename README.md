f96fed3d6a613c580e4633ba84f8bc274479bf74
Pedro Luiz Aroma Filho <pedroaroma@pop-os.localdomain>
2021-03-31 03:56:28 -0300

    Inicialização do projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jdev-Springboot-rest-restful
---
+++ src/main/java/curso/api/rest/CursospringrestapiApplication.java
@@ -0,0 +1,27 @@
+package curso.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"curso.api.rest.model"}) //scaneia as entidades para criação de tabelas no db
+@ComponentScan(basePackages = {"curso.*"}) //configura a injeção de dependência
+@EnableJpaRepositories(basePackages = {"cusro.api.rest.repository"}) //configura as interfaces de dependencias
+@EnableTransactionManagement //configura automaticamente os processos de transaction com o banco para evitar conflitos ao executar queries
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
+public class CursospringrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringrestapiApplication.class, args);
+	}
+
+}
